adaf55809b6b28425aef74f549edbd6f0e63a872
DevModori <junggun.lim@modori.io>
2020-01-15 01:08:42 +0900

    ADD: session - login/logout

--- router/main.js
+++ router/main.js
@@ -10,9 +10,13 @@ module.exports = function(app, fs)
     */
 
     app.get('/', function(req, res) {
+        let sess = req.session;
+
         res.render('index', {
             title: "MY HOMEPAGE",
-            length: 5
+            length: 5,
+            name: sess.name,
+            username: sess.username
         });
     });
 
@@ -119,4 +123,48 @@ module.exports = function(app, fs)
     });
 
     // fs.writeFile(filename, data, [options], callback)
+
+    app.get('/login/:username/:password', function(req, res) {
+        sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data) {
+            let users = JSON.parse(data);
+            let username = req.params.username;
+            let password = req.params.password;
+            let result = { };
+            if(!users[username]) {
+                // USERNAME NOT FOUND
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password) {
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            } else {
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        sess = req.session;
+        if(sess.username) {
+            req.session.destroy(function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.redirect('/');
+                }
+            })
+        } else {
+            res.redirect('/');
+        }
+    })
 }
