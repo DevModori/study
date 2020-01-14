4909eacff150e25716acb49385211e3c64c989a0
DevModori <junggun.lim@modori.io>
2020-01-15 00:51:02 +0900

    ADD: nodeJS 10-2

--- router/main.js
+++ router/main.js
@@ -13,7 +13,7 @@ module.exports = function(app, fs)
         res.render('index', {
             title: "MY HOMEPAGE",
             length: 5
-        })
+        });
     });
 
     app.get('/list', function(req, res) {
@@ -29,4 +29,94 @@ module.exports = function(app, fs)
             res.json(users[req.params.username]);
         });
     });
+
+    app.post('/addUser/:username', function(req, res) {
+
+        let result = { };
+        let username = req.params.username;
+
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]) {
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        // LOAD DATA & CHECK DUPLICATION
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data) {
+            let users = JSON.parse(data);
+            if(users[username]) {
+                // DUPLICATION FOUND
+                result["success"] = 0;
+                result["errer"] = "duplicate";
+                res.json(result);
+                return;
+            }
+            
+            // ADD TO DATA
+            users[username] = req.body;
+
+            // SAVE DATA
+            fs.writeFile(__dirname + "/../data/user.json", JSON.stringify(users, null, '\t'), "utf8", function(err, data) {
+                result = {"success": 1};
+                res.json(result);
+            });
+        });
+    });
+
+    app.put('/updateUser/:username', function(req, res) {
+        let result = { };
+        let username = req.params.username;
+
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]) {
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        // LOAD DATA
+
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data) {
+            let users = JSON.parse(data);
+            // ADD/MODIFY DATA
+            // 있으면 덮어쓰고, 없으면 새로 넣고.
+            users[username] = req.body;
+
+            // SAVE DATA
+
+            fs.writeFile(__dirname + "/../data/user.json", JSON.stringify(users, null, '\t'), 'utf8', function(err, data) {
+                result = { "success": 1 };
+                res.json(result);
+            });
+        });
+    });
+
+    app.delete('/deleteUser/:username', function(req, res) {
+        let result = { };
+
+        // LOAD DATA
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data) {
+            let users = JSON.parse(data);
+
+            // IF NOT FOUND
+            if(!users[req.params.username]) {
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            delete users[req.params.username];
+            fs.writeFile(__dirname + "/../data/user.json", JSON.stringify(users, null, '\t'), 'utf8', function(err, data) {
+                result["success"] = 1;
+                res.json(result);
+                return;
+            });
+        });
+    });
+
+    // fs.writeFile(filename, data, [options], callback)
 }
